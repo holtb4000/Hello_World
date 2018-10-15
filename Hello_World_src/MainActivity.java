496f0f69a7014ea3c7b69d617082319c524a17a3
holtb4000 <holtb4000@gmail.com>
2018-10-15 14:41:01 -0400

    Initial commit

---
+++ Hello_World_src/MainActivity.java
@@ -0,0 +1,41 @@
+//George Holt Brown 
+//M3 Android dummy app
+//added buttons will change viewed text to "i have been triggered" 
+
+package com.example.ian.hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick (View view) {
+                textView.setText("I HAVE BEEN TRIGGERED!");
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
